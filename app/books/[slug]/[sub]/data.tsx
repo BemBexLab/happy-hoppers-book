3a008177236aaadffc0c5bf81a58968a6ccf5a8d
car5p3 <triggred47@icloud.com>
2026-06-25 00:56:36 +0500

    Updateded Content for Book 3

--- app/books/[slug]/[sub]/data.tsx
+++ app/books/[slug]/[sub]/data.tsx
@@ -140,7 +140,7 @@ const quickViewDataByBook: Record<string, QuickViewBookData> = {
       ],
     },
   },
-  
+
   "delicious-treats": {
     "hardcover-books": {
       description:
@@ -151,29 +151,29 @@ const quickViewDataByBook: Record<string, QuickViewBookData> = {
         "Publication year:  2025",
         "Pages: 40",
         "Language: English",
-        "ISBN #:  979-8-3507-3693-9",
+        "ISBN #: 979-8-3507-3897-1",
         "Dimensions:  8.5” w x 11” h",
-        "Weight:  1.0 pound",
+        "Weight: 1.0 pound",
         "Age range: 6 to 99",
         "BISAC Category: Juvenile Fiction",
       ],
       gallery: [
-        "/9bed69_b969bb2967bd4cdfacc87fde8ad56f2f~mv2.avif",
-        "/9bed69_580083f08bb94d0aacc0ecf097cca33d~mv2.avif",
-        "/9bed69_e32809895ab146088244e37dd6e4289d~mv2.avif",
-        "/9bed69_5f336bc8dfcb4abfb5a8997f64b631c3~mv2.avif",
-        "/9bed69_3f7edda29c584daa8e5e05de55e032b8~mv2.avif",
+        "/9bed69_734212ff19de4867a6b19624ed9353cf~mv2.avif",
+        "/9bed69_7ed6f37476854231ae05cba24ee3e52e~mv2.avif",
+        "/9bed69_f7449e9e7f74413a92344894e9e9f138~mv2.avif",
+        "/9bed69_065c1a45654b433e84ccfecd4032eff0~mv2.avif",
+        "/9bed69_c6b8e29dde6f4343b41ca2a4d4d0a578~mv2.avif",
       ],
     },
     "video-books": {
       description:
-        "This is a 09:34 MP4 format video file, lively narrated by Gina Ballotta. Customers receive a link to download the video book after the product is purchased.",
+        "This is a 10:32 MP4 format video file, lively narrated by Gina Ballotta. Customers receive a link to download the video book after the product is purchased.",
       details: [],
       gallery: [
-        "/9bed69_3f7edda29c584daa8e5e05de55e032b8~mv2.avif",
-        "/9bed69_580083f08bb94d0aacc0ecf097cca33d~mv2.avif",
-        "/9bed69_e32809895ab146088244e37dd6e4289d~mv2.avif",
-        "/9bed69_5f336bc8dfcb4abfb5a8997f64b631c3~mv2.avif",
+        "/9bed69_065c1a45654b433e84ccfecd4032eff0~mv2.avif",
+        "/9bed69_7ed6f37476854231ae05cba24ee3e52e~mv2.avif",
+        "/9bed69_f7449e9e7f74413a92344894e9e9f138~mv2.avif",
+        "/9bed69_c6b8e29dde6f4343b41ca2a4d4d0a578~mv2.avif",
       ],
     },
     ebooks: {
@@ -181,37 +181,38 @@ const quickViewDataByBook: Record<string, QuickViewBookData> = {
         "This is a digital Flipbook. Customers receive a link to download the E-book after the product is purchased.",
       details: [],
       gallery: [
-        "/9bed69_3f7edda29c584daa8e5e05de55e032b8~mv2.avif",
-        "/9bed69_580083f08bb94d0aacc0ecf097cca33d~mv2.avif",
-        "/9bed69_e32809895ab146088244e37dd6e4289d~mv2.avif",
-        "/9bed69_5f336bc8dfcb4abfb5a8997f64b631c3~mv2.avif",
+        "/9bed69_065c1a45654b433e84ccfecd4032eff0~mv2.avif",
+        "/9bed69_7ed6f37476854231ae05cba24ee3e52e~mv2.avif",
+        "/9bed69_f7449e9e7f74413a92344894e9e9f138~mv2.avif",
+        "/9bed69_c6b8e29dde6f4343b41ca2a4d4d0a578~mv2.avif",
       ],
     },
     "audio-books": {
       description:
-        "This is a 09:03 MP3 format audio file, lively narrated by Gina Ballotta. Customers receive a link to download the video book after the product is purchased.",
+        "This is a 10:16 MP3 format audio file, lively narrated by Gina Ballotta. Customers receive a link to download the video book after the product is purchased.",
       details: [],
       gallery: [
-        "/9bed69_3f7edda29c584daa8e5e05de55e032b8~mv2.avif",
-        "/9bed69_580083f08bb94d0aacc0ecf097cca33d~mv2.avif",
-        "/9bed69_e32809895ab146088244e37dd6e4289d~mv2.avif",
-        "/9bed69_5f336bc8dfcb4abfb5a8997f64b631c3~mv2.avif",
+        "/9bed69_065c1a45654b433e84ccfecd4032eff0~mv2.avif",
+        "/9bed69_7ed6f37476854231ae05cba24ee3e52e~mv2.avif",
+        "/9bed69_f7449e9e7f74413a92344894e9e9f138~mv2.avif",
+        "/9bed69_c6b8e29dde6f4343b41ca2a4d4d0a578~mv2.avif",
       ],
     },
   },
+
   "thrill-of-exercise": {
     "hardcover-books": {
       description:
         "This is a 4-color, hardcover, printed book. Customers will receive the book via USPS or UPS ground service after the product is purchased.",
       details: [
-        "Artist & Author:  Gina Ballotta",
-        "Publisher:  Happy Hoppers Books",
+        "Artist & Author: Gina Ballotta",
+        "Publisher: Happy Hoppers Books",
         "Publication year:  2025",
         "Pages: 40",
         "Language: English",
-        "ISBN #:  979-8-3507-3693-9",
-        "Dimensions:  8.5” w x 11” h",
-        "Weight:  1.0 pound",
+        "ISBN #:  979-8-3507-3956-5",
+        "Dimensions: 8.5” w x 11” h",
+        "Weight: 1.0 pound",
         "Age range: 6 to 99",
         "BISAC Category: Juvenile Fiction",
       ],
